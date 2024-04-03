bd2610f405f7b59e8efd35ef5bea7c331556d452
Javier Casapia <javiercasapia97@gmail.com>
2024-04-02 23:03:04 -0500

    Read project model data into home.html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from blog import views
+from portfolio import views
 
 app_name = "portfolio"
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+
+from .models import Project
+
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse("Hello World!")
+    projects = Project.objects.prefetch_related('links', 'technologies')
+    return render(request, "portfolio/home.html", {
+        "projects": projects,
+    })
